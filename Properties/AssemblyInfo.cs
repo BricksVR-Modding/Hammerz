20be9480cda69a8a3aa72c272c629083b08a4dcb
ATXLtheAxolotl <49346095+ATXLtheAxolotl@users.noreply.github.com>
2022-01-01 16:42:21 -0500

    Initial Commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using MelonLoader;
+
+[assembly: AssemblyTitle(Ham.BuildInfo.Description)]
+[assembly: AssemblyDescription(Ham.BuildInfo.Description)]
+[assembly: AssemblyCompany(Ham.BuildInfo.Company)]
+[assembly: AssemblyProduct(Ham.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + Ham.BuildInfo.Author)]
+[assembly: AssemblyTrademark(Ham.BuildInfo.Company)]
+[assembly: AssemblyVersion(Ham.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(Ham.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(Ham.HamMod), Ham.BuildInfo.Name, Ham.BuildInfo.Version, Ham.BuildInfo.Author, Ham.BuildInfo.DownloadLink)]
+[assembly: MelonColor()]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame("BricksVR", "BricksVR")]
---
+++ README.md
@@ -0,0 +1,10 @@
+### About
+This mod restores access to the Hammer. It is more optimized than the old hammer, however there is a small chance that you may encounter lag.
+
+### NOTES
+-Will not work in locked rooms.
+-The color slider is a bit buggy in the animation but it still works
+-Most of the time you will need to *yeet* the hammer for it to blow stuff up, however there is a chance that you don't need to yeet it and can keep it in hand
+-The hammer emoji sometimes falls behind the color picker baground, however the button is still functional.
+-Other's cannot see your hammer.
+-Don't use this for griefing. Be responsible with it.
